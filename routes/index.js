88da2af6228754b9704bafa90bd62a270c2f82dd
Son Truong <sont85@gmail.com>
2015-08-02 01:26:26 -0700

    created log out button

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,8 @@ var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Clarity' });
+  console.log(req.user)
+  res.render('index', { user: req.user });
 });
 
 
