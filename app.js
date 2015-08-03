24b7dd27ee3346eeff83818de53407521efe2a3f
Son Truong <sont85@gmail.com>
2015-08-02 17:29:06 -0700

    refactor passport into config passport file

--- app.js
+++ app.js
@@ -7,59 +7,14 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var io = require('socket.io')();
 var session = require('express-session');
-var passport = require('passport');
-var Teacher = require('./models/teacherSchema');
-var Student = require('./models/studentSchema');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 var app = express();
 app.io = io;
 
 require('./io')(io);
 
-
 var routes = require('./routes/index');
 var teacher = require('./routes/teacher');
-
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback',
-    passReqToCallback: true
-  },
-  function(req, accessToken, refreshToken, profile, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      Student.findOne({email: profile.emails[0].value}, function(err, student){
-        if (student) {
-          profile.type = student.type;
-          console.log('=======student========',profile);
-          return done(null, profile);
-        } else {
-          Teacher.findOne({email: profile.emails[0].value}, function(err, teacher){
-            if (teacher) {
-              profile.type = teacher.type;
-              console.log('=========teacher=======', profile);
-              return done(null, profile);
-            } else {
-              return done(null, profile);
-            }
-          });
-        }
-      });
-
-    });
-  }
-));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -74,10 +29,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({ secret: process.env.CLARITY_SECRET, resave: false, saveUninitialized: true }));
-// Initialize Passport!  Also use passport.session() middleware, to support
-// persistent login sessions (recommended).
-app.use(passport.initialize());
-app.use(passport.session());
+
+require('./config/passport')(app);
 
 app.use('/', routes);
 app.use('/teacher', teacher);
---
+++ config/passport.js
@@ -0,0 +1,47 @@
+'use strict';
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var Teacher = require('../models/teacherSchema');
+var Student = require('../models/studentSchema');
+
+module.exports = function(app){
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+
+  passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+  });
+
+  passport.use(new GoogleStrategy({
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/auth/google/callback',
+      passReqToCallback: true
+    },
+    function(req, accessToken, refreshToken, profile, done) {
+      process.nextTick(function () {
+        Student.findOne({email: profile.emails[0].value}, function(err, student){
+          if (student) {
+            profile.type = student.type;
+            console.log('=======student========',profile);
+            return done(null, profile);
+          } else {
+            Teacher.findOne({email: profile.emails[0].value}, function(err, teacher){
+              if (teacher) {
+                profile.type = teacher.type;
+                console.log('=========teacher=======', profile);
+                return done(null, profile);
+              } else {
+                return done(null, profile);
+              }
+            });
+          }
+        });
+      });
+    }
+  ));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+};
